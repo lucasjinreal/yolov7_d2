6ad3c2dce8962e971e83e19c1e98264e67539349
Your Name <you@example.com>
2021-11-04 12:01:12 +0800

    add smca

--- yolov7/modeling/meta_arch/smca_detr.py
+++ yolov7/modeling/meta_arch/smca_detr.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import logging
 import math
-from typing import List
+from typing import List, Dict
 
 import numpy as np
 import torch
@@ -27,6 +27,75 @@ from ..backbone.smcadetr_backbone import Joiner, PositionEmbeddingSine, Transfor
 from .detr_seg import DETRsegm, PostProcessPanoptic, PostProcessSegm, sigmoid_focal_loss, dice_loss
 
 __all__ = ["Detr"]
+
+
+class MaskedBackboneTraceFriendly(nn.Module):
+    """ 
+    This is a thin wrapper around D2's backbone to provide padding masking.
+    I change it into tracing friendly with this mask operation.
+    """
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.backbone = build_backbone(cfg)
+        backbone_shape = self.backbone.output_shape()
+        self.feature_strides = [
+            backbone_shape[f].stride for f in backbone_shape.keys()]
+        self.num_channels = backbone_shape[list(
+            backbone_shape.keys())[-1]].channels
+        self.onnx_export = False
+
+    def forward(self, images):
+        if isinstance(images, ImageList):
+            features = self.backbone(images.tensor)
+            device = images.tensor.device
+        else:
+            features = self.backbone(images.tensors)
+            device = images.tensors.device
+
+        if self.onnx_export:
+            logger.info('[onnx export] in MaskedBackbone...')
+            out: Dict[str, NestedTensor] = {}
+            for name, x in features.items():
+                m = images.mask
+                print('m: ', m)
+                print('m: ', m.shape)
+                assert m is not None
+                sp = x.shape[-2:]
+                # mask = F.interpolate(m.to(torch.float), size=sp).to(torch.bool)[0]
+                # mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
+                m = m.unsqueeze(0).float()
+                mask = F.interpolate(m, size=x.shape[-2:]).to(torch.bool)[0]
+                print(mask.shape)
+                out[name] = NestedTensor(x, mask)
+            return out
+        else:
+            masks = self.mask_out_padding(
+                [features_per_level.shape for features_per_level in features.values()],
+                images.image_sizes,
+                device,
+            )
+            assert len(features) == len(masks)
+            for i, k in enumerate(features.keys()):
+                features[k] = NestedTensor(features[k], masks[i])
+            return features
+
+    def mask_out_padding(self, feature_shapes, image_sizes, device):
+        masks = []
+        assert len(feature_shapes) == len(self.feature_strides)
+        for idx, shape in enumerate(feature_shapes):
+            N, _, H, W = shape
+            masks_per_feature_level = torch.ones(
+                (N, H, W), dtype=torch.bool, device=device)
+            for img_idx, (h, w) in enumerate(image_sizes):
+                # print('H', H, 'W', W, 'ceil: ', int(np.ceil(float(h) / self.feature_strides[idx])),)
+                masks_per_feature_level[
+                    img_idx,
+                    : int(np.ceil(float(h) / self.feature_strides[idx])),
+                    : int(np.ceil(float(w) / self.feature_strides[idx])),
+                ] = 0
+            masks.append(masks_per_feature_level)
+        return masks
 
 
 class MaskedBackbone(nn.Module):
